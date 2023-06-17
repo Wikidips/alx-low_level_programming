bce578fd51e294702519c1539a6e8b646512de7e
clear <wikidips18@gmail.com>
2023-06-17 14:26:35 +0100

    vlbhj

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,14 @@
       */
 int main(void)
 {
-		int i;
+	    int i;
 
-			for (i = 48; i < 58; i++)
-					{
-								putchar(i);
-									}
-				putchar('\n');
-					return (0);
+	        for (i = 48; i < 58; i++)
+			    {
+				            putchar(i);
+					        }
+
+		    putchar('\n');
+		        return (0);
 }
 
