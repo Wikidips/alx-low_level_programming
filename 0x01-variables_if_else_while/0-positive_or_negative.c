74c8891a08acb96ca4d9ba6da7a522ca7a40617a
clear <wikidips18@gmail.com>
2023-06-17 02:30:30 +0100

    vhjh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -9,16 +9,20 @@ nclude <stdlib.h>
       */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}	
+		int n;
+		srand(time(0));
+			n = rand() - RAND_MAX / 2;
+				if (n > 0)
+						{
+									printf("%d is positive\n", n);
+										}
+					else if (n == 0)
+							{
+										printf("%d is zero\n", n);
+											}
+						else
+								{
+											printf("%d is negative\n", n);
+												}
+							return (0);
+}							
