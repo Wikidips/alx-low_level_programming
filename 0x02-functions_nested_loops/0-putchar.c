7c7582aef635eb0067421ea420b74b40d890c045
Wikidips <wikidips18@gmail.com>
2023-06-20 12:13:21 -0700

    ggh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
-#include <unistd.h>
 #include "main.h"
-/**
+	#include <unistd.h>
+	/**
 	 * _putchar - writes the character c to stdout
 	 * @c: The character to print
 	 *
@@ -11,4 +11,4 @@
 	{
 		return (write(1, &c, 1));
 	}
-"
+
