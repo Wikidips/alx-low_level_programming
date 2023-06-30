7b9993af95d815aa9e553792f61a608679ee7fb5
Wikidips <wikidips18@gmail.com>
2023-06-29 19:13:56 -0700

    easy on me

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,23 +5,24 @@
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
-	int F = 0;
+	int i = 0;
 	int j = 0;
 	char temp;
 
-	while (*(n + F) != '\0')
+	while (*(n + i) != '\0')
 	{
-		F++;
+		i++;
 	}
-	F--;
+	i--;
 
-	for (j = 0; j < F; j++, F--)
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
-		*(n + j) = *(n + F);
-		*(n + F) = temp;
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
@@ -33,52 +34,44 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, F = 0, j = 0, digits = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + F) != '\0')
-		F++;
+	while (*(n1 + i) != '\0')
+		i++;
 	while (*(n2 + j) != '\0')
 		j++;
-	F--;
+	i--;
 	j--;
-
-	if (j >= size_r || F >= size_r)
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-	while (j >= 0 || F >= 0 || overflow == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (F < 0)
+		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + F) - '0';
-
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-
 		temp_tot = val1 + val2 + overflow;
-
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
 			overflow = 0;
-
 		if (digits >= (size_r - 1))
 			return (0);
-
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
-		F--;
+		i--;
 	}
-
 	if (digits == size_r)
 		return (0);
-
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
