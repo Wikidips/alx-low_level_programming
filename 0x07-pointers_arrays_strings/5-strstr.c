fa25c65a9ec2a0f1a0aa6a1138b94101a7971495
Wikidips <wikidips18@gmail.com>
2023-07-03 19:06:30 -0700

    halala

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: the second input
+ * @needle: this is the input 
+ * return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle;
+
+		while (*i == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
