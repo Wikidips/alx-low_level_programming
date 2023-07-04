bcccf0e119d7bab08d9508ce75097156c954929a
Wikidips <wikidips18@gmail.com>
2023-07-03 19:10:07 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,12 +9,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *i = haystack;
+		char *l = haystack;
 		char *p = needle;
 
-		while (*i == *p && *p != '\0')
+		while (*l == *p && *p != '\0')
 		{
-			i++;
+			l++;
 			p++;
 		}
 
