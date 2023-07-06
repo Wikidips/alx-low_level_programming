7b5aeddd05b3949649c96c812fb418456b44ce0d
Wikidips <wikidips18@gmail.com>
2023-07-05 17:43:08 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - the function that returns the value of x raised to the power of y
+ * _pow_recursion - to return the value of x raised to the power of y
  * @x: the value to raise
  * @y: the power of x
  *
