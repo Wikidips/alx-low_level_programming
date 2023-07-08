ad38dfabcbdfcaf60a6f78916cd5b1a44f8b0f3a
Wikidips <wikidips18@gmail.com>
2023-07-08 10:36:02 -0700

    evrything

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Description: This function prints all the command-line arguments
+ *
+ * Return: 0 (indicating successful execution)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
