20088f2c202f8add61a11f4db4979de0d06c9c48
Wikidips <wikidips18@gmail.com>
2023-07-07 19:54:25 -0700

    non

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
  *
- * Return: Always 0 (Success)
+ * Description: This function prints the program name
+ *
+ * Return: 0 (indicating successful execution)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
-
