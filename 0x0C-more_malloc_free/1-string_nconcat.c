e3b233c1d007791c5287baf17936061ace67af05
Wikidips <wikidips18@gmail.com>
2023-07-13 08:07:56 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,9 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * @b: int
  *
  * Return: a pointer to the allocated memory
  */
