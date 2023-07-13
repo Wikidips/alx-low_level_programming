37a28f1e23fe2d5b2f9251071d6958b79f6f2795
Wikidips <wikidips18@gmail.com>
2023-07-13 08:11:51 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,8 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: int
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
