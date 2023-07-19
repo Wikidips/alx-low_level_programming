ccad73507966d5839439fd2731740d7d6bc25a42
Wikidips <wikidips18@gmail.com>
2023-07-19 15:09:15 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,23 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * array_iterator - Executes a function on each element of an array.
- * 
- * @array: Array to execute func on.
- * @size: Size of the array.
- * @action: Pointer to the function you need to use.
+ * array_iterator - prints each array element on a new line
+ * @array: array
+ * @size: elements to print
+ * @action: pointer to print.
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array != NULL && action != NULL)
-    {
-        for (size_t i = 0; i < size; i++)
-        {
-            action(array[i]);
-        }
-    }
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
 
