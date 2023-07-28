f578c48a24269dc7464b0282e98f7c649ff0add9
wikidips <Wikidips18@gmail.com>
2023-07-27 19:57:25 -0700

    zd

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
-#include "lists.h"
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
 
 /**
- * before_main - Function that prints a message.
- * function to be executed.
- * Return: Void
+ * first - prints a message before the
+ * function is executed
  */
-
-void __attribute__((constructor)) before_main()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
