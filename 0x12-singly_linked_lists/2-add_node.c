a5212f5d5b50a5ed4dda3cb8f0fc1ca86356fadf
wikidips <Wikidips18@gmail.com>
2023-07-27 19:13:40 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,29 +3,25 @@
 #include "lists.h"
 
 /**
- * add_node - func that adds a new node at the beginning of a linked list
+ * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
- * Return: the address of the new element, or NULL if it didn't work
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+	list_t *new_node;
+	unsigned int len = 0;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	while (str[len])
+		len++;
 
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
 
