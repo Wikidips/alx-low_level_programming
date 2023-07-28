e454a4e7dccdb99d76c4fb72f7d66ffe69e4fb68
wikidips <Wikidips18@gmail.com>
2023-07-27 19:27:31 -0700

    se

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,29 +6,23 @@
  * add_node_end - adds a new node at the end of a linked list
  * @head: double pointer to the list_t list
  * @str: string to add in the new node
- * Return: address of the new element, or NULL if it failed
+ * Return: address of the new_node element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+	list_t *new_node;
+	list_t *tmp = *head;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = 0;
-	while (str[new_node->len])
-		new_node->len++;
-
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -37,12 +31,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
-	list_t *temp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
 
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new_node;
+	tmp->next = new_node;
 
 	return (new_node);
 }
