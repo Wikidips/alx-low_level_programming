8af8f040acad4af9650b3504933bc3a94b423bf7
Wikidips <Wikidips18@gmail.com>
2023-08-01 22:44:05 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,17 +2,20 @@
 
 /**
  * free_listint2 - Frees a listint_t list and sets head to NULL.
- * @head: Pointer to the head of the list.
+ * @head: Double pointer to the head of the linked list.
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *NODE;
+	listint_t *tmp;
 
-	while (*head != NULL)
+	if (!head)
+		return;
+
+	while (*head)
 	{
-		NODE = *head;
-		*head = (*head)->next;
-		free(NODE);
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 }
 
