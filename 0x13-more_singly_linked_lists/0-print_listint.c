042287c5c4fc5fa8a9f2250cabef1d17fb4626f0
Wikidips <Wikidips18@gmail.com>
2023-08-01 15:46:05 +0100

    refix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: pointer that points to the linked list
+ * @h: pointer to the list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t d = 0;
+	size_t Q = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		d++;
+		Q++;
 		h = h->next;
 	}
 
-	return (d);
+	return (Q);
 }
+
