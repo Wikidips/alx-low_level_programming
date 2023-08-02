c4b8970a1680a92cb97ad4d92d8e8c6d9fcf6d0a
Wikidips <Wikidips18@gmail.com>
2023-08-02 03:31:22 +0100

    l3adaw

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t linked list, handling loops.
+ * @h: Pointer to a pointer to the head of the linked list.
+ *
+ * Return: The size of the list that has been freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *next;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+	while (current)
+	{
+		count++;
+		next = current->next;
+		free(current);
+
+		if (current <= next)
+		{
+			*h = NULL;
+			return (count);
+		}
+
+		current = next;
+	}
+
+	*h = NULL;
+	return (count);
+}
+
