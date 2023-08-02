8956d9667d87989a82b0420675f3d939948ab797
Wikidips <Wikidips18@gmail.com>
2023-08-02 01:59:42 +0100

    mad

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,43 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at a given position.
+ * delete_nodeint_at_index - Deletes the node at a given position
+ * in the linked list.
  * @head: Pointer to the head of the list.
  * @index: The index of the node to be deleted.
  *
  * Return: 1 if it succeeded, or -1 if it failed.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *current, *tmp;
+	unsigned int c = 0;
+
 	if (!head || !*head)
 		return (-1);
 
-	listint_t *current = *head;
+	current = *head;
 
-	listint_t *prev = NULL;
-	unsigned int i = 0;
-
-	while (current)
+	if (index == 0)
 	{
-		if (i == index)
-		{
-			if (prev)
-				prev->next = current->next;
-			else
-				*head = current->next;
-			free(current);
-			return (1);
-		}
-
-		prev = current;
-		current = current->next;
-		i++;
+		*head = current->next;
+		free(current);
+		return (1);
 	}
 
+	while (current != NULL)
+	{
+		if (c == index - 1)
+		{
+			tmp = current->next;
+			if (tmp == NULL)
+				return (-1);
+			current->next = tmp->next;
+			free(tmp);
+			return (1);
+		}
+		current = current->next;
+		c++;
+	}
 	return (-1);
 }
-
