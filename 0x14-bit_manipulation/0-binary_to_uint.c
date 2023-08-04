2ba19c1854438de62d7676c4dfbed29ec33b72f9
Wikidips <Wikidips18@gmail.com>
2023-08-04 02:07:43 +0100

    not working

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: The binary string to convert.
+ * binary_to_uint - Write a function that converts a binary to int.
+ * @b: char to be printed
  *
- * Return: The converted number, or 0 if there are invalid characters.
- */
+ * Return: c SUCCESS the final char
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int c;
+	int len, secondbase;
 
-	if (!b)
+	c = 0;
+	len = 0;
+	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
+	while (b[len] != '\0')
+		len++;
+	for (len--, secondbase = 1; len >= 0; len--, secondbase *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-
-		result = (result * 2) + (b[i] - '0');
+		if (b[len] & 1)
+			c += secondbase;
 	}
-
-	return (result);
+	return (c);
 }
-
