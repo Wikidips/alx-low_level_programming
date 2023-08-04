7889307847919fd0bbf00e82b796b58c8cf16781
Wikidips <Wikidips18@gmail.com>
2023-08-04 01:28:18 +0100

    start

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,25 +2,25 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * @b: The binary string to convert.
  *
  * Return: The converted number, or 0 if there are invalid characters.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int output = 0;
+	unsigned int result = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (; *b; b++)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if (*b == '0' || *b == '1')
-			output = (output << 1) | (*b - '0');
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
+		result = (result * 2) + (b[i] - '0');
 	}
 
-	return (output);
+	return (result);
 }
 
