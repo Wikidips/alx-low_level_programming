a552d2586613926549ee4d2ab70c2960b755a314
Wikidips <Wikidips18@gmail.com>
2023-08-04 15:09:26 +0100

    still test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: binary.
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: binary
  *
- * Return: unsigned int.
+ * Return: The unsigned int equivalent of the binary number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len;
-	unsigned int result = 0;
-
 	if (!b)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+	unsigned int result = 0;
 
-	for (len--; len >= 0; len--)
+	for (int i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] == '0')
+		{
+			result = (result << 1);
+		}
+		else if (b[i] == '1')
+		{
+			result = (result << 1) | 1;
+		}
+		else
 		{
 			return (0);
-		}
-
-		if (b[len] == '1')
-		{
-			result |= (1 << (len));
 		}
 	}
 
 	return (result);
 }
+
