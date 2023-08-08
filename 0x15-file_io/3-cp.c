5792d12b765e28af6e90d3669755ca4f2cc7001e
Wikidips <Wikidips18@gmail.com>
2023-08-08 02:50:01 +0100

    the law according to lidia poet

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,65 @@
 #include "main.h"
 
 /**
- * error_check_files - checks if files can be opened.
+ * main - Copies the content of one file to another.
  *
- * @src_fd: source file descriptor.
- * @dest_fd: destination file descriptor.
- * @args: arguments vector.
+ * @argc: The argument count.
+ * @argv: The argument vector.
  *
- * Return: 0 if sucssess
+ * Return: 0 on success, otherwise exits with an error code.
  */
-void error_check_files(int src_fd, int dest_fd, char *args[])
+int main(int argc, char **argv)
 {
-	if (src_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
-		exit(98);
-	}
-	if (dest_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]);
-		exit(99);
-	}
-}
-
-/**
- * main - check the code for ALX students
- *
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	int src_fd, dest_fd, err_close;
-	ssize_t num_chars, num_written;
+	int src_fd, dest_fd, read_count;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	src_fd = open(argv[1], O_RDONLY);
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_check_files(src_fd, dest_fd, argv);
-
-	num_chars = 1024;
-	while (num_chars == 1024)
+	if (src_fd == -1)
 	{
-		num_chars = read(src_fd, buffer, 1024);
-		if (num_chars == -1)
-			error_check_files(-1, 0, argv);
-		num_written = write(dest_fd, buffer, num_chars);
-		if (num_written == -1)
-			error_check_files(0, -1, argv);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	err_close = close(src_fd);
-	if (err_close == -1)
+	dest_fd = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (dest_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((read_count = read(src_fd, buffer, 1024)) > 0)
+	{
+		if (write(dest_fd, buffer, read_count) != read_count)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (read_count == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(src_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
 		exit(100);
 	}
 
-	err_close = close(dest_fd);
-	if (err_close == -1)
+	if (close(dest_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
 		exit(100);
 	}
+
 	return (0);
 }
+
