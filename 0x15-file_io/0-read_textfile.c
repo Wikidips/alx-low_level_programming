f7c5c50c4c031599b173bd3026fde53a1ec91492
Wikidips <Wikidips18@gmail.com>
2023-08-08 01:58:22 +0100

    hoping

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,53 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - Reads a text file and prints it to the standard output
+ * @filename: The name of the file
+ * @letters: The number of letters to read and print
  *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * Return: The number of letters actually read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer = NULL;
+	int _open, _read, _write;
+	char *buffer;
+	ssize_t printed_letters = 0;
 
 	if (!filename)
 		return (0);
 
-	buffer = malloc(letters + 1);
+	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		goto cleanup;
+	_open = open(filename, O_RDONLY);
+	if (_open == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-		goto cleanup_fd;
+	_read = read(_open, buffer, letters);
+	if (_read == -1)
+	{
+		free(buffer);
+		close(_open);
+		return (0);
+	}
 
-	buffer[bytes_read] = '\0';
+	_write = write(STDOUT_FILENO, buffer, _read);
+	if (_write == -1 || _write != _read)
+	{
+		free(buffer);
+		close(_open);
+		return (0);
+	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		goto cleanup_fd;
+	printed_letters = _write;
 
-	close(fd);
 	free(buffer);
-	return (bytes_written);
+	close(_open);
 
-cleanup_fd:
-	close(fd);
-cleanup:
-	free(buffer);
-	return (0);
+	return (printed_letters);
 }
 
