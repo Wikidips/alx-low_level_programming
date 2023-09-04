a58dbe133548bf854ee74f0226fc2cd00b731263
Wikidips <wikidips@gmail.com>
2023-09-04 20:56:44 +0100

    do

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,37 @@
 #include "main.h"
-/**
- * main - function declaration
- *
- * @argc: arguments
- * @argv: double pointer
- *
- * Return: letters
-*/
-int main(int argc, char **argv)
-{
-	int char1, char2, i;
-	char buffer[1024];
 
-	if (argc != 3)
+/**
+ * read_textfile - a function that reads a
+ * text file and prints it to the POSIX standard output
+ * @filename: of the file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	register int _open, _read, _write;
+	register char *buffer = malloc(sizeof(char)  * letters);
+
+	if (!(buffer))
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		free(buffer);
+		return (0);
 	}
-	char1 = open(argv[1], O_RDONLY);
-	if (char1 == -1)
+	if (!(filename))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		return (0);
 	}
-	char2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	while ((i = read(char1, buffer, 1024)) > 0)
+	_open = open(filename, O_RDONLY);
+	_read = read(_open, buffer, letters);
+	_write = write(STDOUT_FILENO, buffer, _read);
+
+	if (_open == -1 || _read == -1 || _write == -1 || !(_write == _read))
 	{
-		if (write(char2, buffer, i) != i || char2 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		free(buffer);
+		return (0);
 	}
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(char1) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", char1);
-		exit(100);
-	}
-	if (close(char2) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", char2);
-		exit(100);
-	}
-	return (0);
+	free(buffer);
+	close(_open);
+	return (_write);
 }
